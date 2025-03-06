8ec45cf545d2e550d51f2f09277a050879a8331b
Nico29061 <trithm29122006@gmail.com>
2025-03-06 09:52:08 +0700

    Thay đổi code, thêm ảnh image.bmp
    Tạo các hàm riêng thay vì để trong hàm main

--- Echoing_Maze.cpp
+++ Echoing_Maze.cpp
@@ -4,53 +4,78 @@
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 
+//ham khoi tao SDL
+bool initSDL(){
+    std::cout << "Dang khoi tao SDL..." << std::endl;
+    if (SDL_Init(SDL_INIT_VIDEO) < 0){
+        std::cout << "SDL khong the khoi tao!! Loi: " << SDL_GetError() << std::endl;
+        return false;
+    }
+    std::cout << "SDL khoi tao thanh cong!" << std::endl;
+    return true;
+}
+
+//ham tao cua so
+SDL_Window* createWindow(){
+    std::cout << "Dang tao cua so..." << std::endl;
+    SDL_Window* window = SDL_CreateWindow(
+        "Echoing Maze",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        SCREEN_WIDTH, SCREEN_HEIGHT,
+        SDL_WINDOW_SHOWN
+    );
+
+    if (window == nullptr){
+        std::cout << "Khong the khoi tao cua so! Loi: " << SDL_GetError() << std::endl;
+    } else {
+        std::cout << "Khoi tao cua so thanh cong!" << std::endl;
+    }
+
+    return window;
+}
+
+//ham render
+SDL_Renderer* createRenderer(SDL_Window* window){
+    std::cout << "Dang render..." << std::endl;
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    
+    if (renderer == nullptr){
+        std::cout << "Khong the render!! Loi: " << SDL_GetError() << std::endl;
+    } else {
+        std::cout << "Da render thanh cong!" << std::endl;
+    }
+    return renderer;
+}
+
 int main(int argc, char* argv[]){
     //khoi tao sdl
-    if (SDL_Init(SDL_INIT_VIDEO) < 0){
-        std::cout << "LOI HE THONG !! KHONG THE KHOI TAO !! LOI: " << SDL_GetError() << std::endl;
+    if (!initSDL()){
         return -1;
     }
-    std::cout << "SDL khoi tao thanh cong" << std::endl;
 
-    //tao cua so (window)
-
-    std::cout << "Dang tao cua so..." << std::endl;
-    SDL_Window* window = SDL_CreateWindow("Echoing Maze", SDL_WINDOWPOS_CENTERED, 
-    SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    
+    //tao cua so
+    SDL_Window* window = createWindow();
     if (window == nullptr){
-        std::cout << "KHONG THE TAO CUA SO!! LOI: " << SDL_GetError();
         SDL_Quit();
         return -1;
     }
-    std::cout << "Cua so da tao thanh cong" << std::endl;
 
-    //tao renderer
-
-    std::cout << "Dang tao renderer..." << std::endl;
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    //render
+    SDL_Renderer* renderer = createRenderer(window);
     if (renderer == nullptr){
-        std::cout << "KHONG THE RENDER MAN HINH!! LOI: " << SDL_GetError();
         SDL_DestroyWindow(window);
-        SDL_Quit();
+        SDL_Quit;
         return -1;
     }
-    std::cout << "Tao renderer thanh cong" << std::endl;
-
-    //mau nen cua so
-
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
-    std::cout << "Man hinh da ve xong" << std::endl;
 
     SDL_Delay(3000);
 
-    //don dep tai nguyen
-    std::cout << "Dang giai phong tai nguyen..." << std::endl;
+    //giai phong tai nguyen
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
-    std::cout << "Chuong trinh da ket thuc!" << std::endl;
+    std::cout << "Chuong trinh da ket thuc" << std::endl;
+
     return 0;
 }
