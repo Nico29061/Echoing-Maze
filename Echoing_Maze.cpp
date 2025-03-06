3e6393da418a96f08035b91f3d6e31d6719fa79e
Nico29061 <trithm29122006@gmail.com>
2025-03-06 17:50:19 +0700

    Cập nhật Code, bổ sung thêm Hàm
    Thêm SDL_Image, SDL_ttf, bổ sung code thêm hàm v.v

--- Echoing_Maze.cpp
+++ Echoing_Maze.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
@@ -12,8 +13,17 @@ bool initSDL(){
         return false;
     }
     std::cout << "SDL khoi tao thanh cong!" << std::endl;
+
+    std::cout << "Dang khoi tao SDL Image..." << std::endl;
+    if (IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG) == 0){
+        std::cout << "Khong the khoi tao SDL_image! Loi: " << IMG_GetError() << std::endl;
+        return false;
+    }
+    std::cout << "SDL Image khoi tao thanh cong!" << std::endl;
+
     return true;
 }
+//ket thuc phan ham khoi tao SDL
 
 //ham tao cua so
 SDL_Window* createWindow(){
@@ -34,6 +44,7 @@ SDL_Window* createWindow(){
 
     return window;
 }
+//ket thuc phan ham tao window
 
 //ham render
 SDL_Renderer* createRenderer(SDL_Window* window){
@@ -47,8 +58,69 @@ SDL_Renderer* createRenderer(SDL_Window* window){
     }
     return renderer;
 }
+//ket thuc phan ham render
+
+//ham texture
+SDL_Texture* loadTexture(SDL_Renderer* renderer, const std::string& path){
+    std::cout << "Dang tai texture..." << std::endl;
+    SDL_Surface* tempSurface = IMG_Load(path.c_str());
+    if (tempSurface == nullptr){
+        std::cout << "Khong the tai hinh anh! Loi: " << IMG_GetError() << std::endl;
+        return nullptr;
+    }
+
+    std::cout << "Dang tai hinh anh..." << std::endl;
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, tempSurface);
+    SDL_FreeSurface(tempSurface);
+    if (texture == nullptr){
+        std::cout << "Khong the tao texture! Loi: " << SDL_GetError() << std::endl;
+    } else {
+        std::cout << "Tao hinh anh thanh cong!" << std::endl;
+    }
+
+    return texture;
+}
+
+//ham gameloop, su kien cua game
+void handleEvents(bool &running){
+    SDL_Event event;
+    while (SDL_PollEvent(&event)){
+        if (event.type == SDL_QUIT){
+            running = false;
+        } else if (event.type == SDL_KEYDOWN){
+            std::cout << "Phim nhan: " << event.key.keysym.sym << std::endl;
+        }
+    }
+}
+
+void render(SDL_Renderer* renderer, SDL_Texture* texture){
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+    SDL_RenderPresent(renderer);
+}
+
+void updateGame() {
+
+}
+
+void gameLoop(SDL_Renderer* renderer, SDL_Texture* texture){
+    std::cout << "Game dang chay..." << std::endl;
+    bool running = true;
+    while (running){
+        handleEvents(running);
+
+        updateGame();
+
+        render(renderer, texture);
+
+        SDL_Delay(16);
+    }
+}
+//ket thuc phan ham vong lap
+
 
 int main(int argc, char* argv[]){
+    std::cout << "Khoi dong console" << std::endl;
     //khoi tao sdl
     if (!initSDL()){
         return -1;
@@ -65,17 +137,28 @@ int main(int argc, char* argv[]){
     SDL_Renderer* renderer = createRenderer(window);
     if (renderer == nullptr){
         SDL_DestroyWindow(window);
-        SDL_Quit;
+        SDL_Quit();
         return -1;
     }
 
-    SDL_Delay(3000);
+    //tai texture
+    SDL_Texture* texture = loadTexture(renderer, "resources/images/White_Screen.bmp");
+    if (texture == nullptr){
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
+
+    //chay vong lap
+    gameLoop(renderer, texture);
 
     //giai phong tai nguyen
+    SDL_DestroyTexture(texture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
-    std::cout << "Chuong trinh da ket thuc" << std::endl;
+    IMG_Quit();
 
     return 0;
 }
